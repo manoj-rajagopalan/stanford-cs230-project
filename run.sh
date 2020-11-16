927c39279a5984351b7d1a9aa35bc4038e1c938b
Manoj Rajagopalan <rajagopalan.manoj@gmail.com>
2020-11-16 08:15:57 +0000

    Tweak PYTHONPATH in run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ if [[ "x${conda_env}x" != "xpytorch_latest_p37x" ]]; then
 	exit 1
 fi
 
-PYTHONPATH=$PYTHONPATH:$PWD \
+PYTHONPATH=$PWD:$PYTHONPATH \
 python3 luo16/train_test.py --data-path kitti_2015 --exp-name plumbing --batch-size 10 --learning-rate 0.01 --reduction-factor 1 --phase both --patch-size 13 --test-all TEST_ALL --max-batches 10
 
