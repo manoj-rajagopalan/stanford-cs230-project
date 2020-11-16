4fefa58a0ee6f2211c6d8b20e0faddda03900ce8
Manoj Rajagopalan <rajagopalan.manoj@gmail.com>
2020-11-16 04:35:19 +0000

    Add default run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+conda_env=$(conda info | grep 'active environment' | cut -d ':' -f 2 | xargs)
+if [[ "x${conda_env}" == "xNone" ]]; then
+	echo "Cannot run without conda env: conda activate pytorch_latest_p37"
+	exit 1
+fi
+
+PYTHONPATH=$PYTHONPATH:$PWD \
+python3 luo16/train_test.py --data-path kitti_2015 --exp-name plumbing --batch-size 10 --learning-rate 0.01 --reduction-factor 1 --phase both --patch-size 13 --test-all TEST_ALL --max-batches 10
+
