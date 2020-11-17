f6f7cd7493f96cca2af2565ef2db8fd62758a92e
Manoj Rajagopalan <rajagopalan.manoj@gmail.com>
2020-11-17 10:49:14 -0800

    Relabel einsum indices in, and add notes to, inner_product_loss

--- loss/inner_product_loss.py
+++ loss/inner_product_loss.py
@@ -15,29 +15,43 @@ class InnerProductLoss(Module):
     def forward(self, left_feature, right_feature, labels):
         """Calculate the loss describe above.
 
+        Legend:
+          B : number of mini-batches
+          F : number of features (eg: 64)
+          D : disparity range (eg: 201)
+
         Args:
           left_feature (Tensor): output of from the left patch passing through
-                                 the Siamese network of dimensions (1, 1, 1, 64)
+                                 the Siamese network of dimensions (B, F, 1, 1)
           right_feature (Tensor): output of from the right patch passing through
                                   the Siamese network of dimensions
-                                  (1, 1, disparity_range, 64)
-          lables (Tensor): the "ground truth" of the expected disparity for the
+                                  (B, F, 1, D)
+          labels (Tensor): the "ground truth" of the expected disparity for the
                            patches of dimensions (1, disparity_range)
-
 
         Returns:
           left_patch (Tensor): sub-image left patch
           right_patch (Tensor): sub-image right patch
           labels (numpy array): the array used as "ground truth"
+
+        Notes:
+          * Each sample is a pixel
+          * At input, left-patch is a receptive field around the sample pixels
+            Right-patch is a larger receptive field (accounting for disparity)
+          * This function is at the end of the network.
+            By this point, each pixel is mapped to a feature vector for the left-patch
+            and D feature-vectors (one per disparity candidate) for the right-patch.
+          * There are B mini-batches of such data.
         """
         left_feature = torch.squeeze(left_feature)
+        right_feature = torch.squeeze(right_feature)
         # perform inner product of left and right features
-        inner_product = torch.einsum('il,iljk->ik', left_feature, right_feature)
+        inner_product = torch.einsum('bf,bfd->bd', left_feature, right_feature)
         # peform the softmax with logits in two steps.  torch does not support
         # softmax with logits on float labels, so the calculation is broken
         # into calculating yhat and then the loss
         yhat = F.log_softmax(inner_product, dim=-1)
-        loss = -1.0 * torch.einsum('ij,ij->i', yhat, labels).sum() / yhat.size(0)
+        loss = -1.0 * torch.einsum('bd,bd->b', yhat, labels).sum() / yhat.size(0)
 
         return loss
 
